745def632ee4884debbb3945d72ca57f54c1609e
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-09-20 00:07:57 -0300

    Day_04
    Challenge project - day 4

---
+++ day04/rock_paper_scissors.py
@@ -0,0 +1,57 @@
+import random
+import rps_art_game
+
+# The Basic Rules of RPS
+
+# Despite its underlying complexity, the game’s rules are straightforward. Players deliver hand signals representing rock, paper, or scissors, 
+# with the outcome determined by these three rules:
+
+# Rock wins against scissors.
+# Scissors win against paper.
+# Paper wins against rock.
+
+print("*** Welcome to Rock, Paper or Scissors game ***")
+print("What do you choose: Type 0 for Rock, 1 for Paper or 2 for Scissors!")
+user_input = int(input(">>> "))
+computer_input = random.randint(0, 2)
+
+if user_input == 0 or user_input == 1 or user_input == 2:
+    
+    print("\nYOU:")
+    
+    if user_input == 0:
+        rps_art_game.rock()
+    elif user_input == 1:
+        rps_art_game.paper()
+    else:
+        rps_art_game.scissors()
+        
+    print("COMPUTER:")
+    
+    if computer_input == 0:
+        rps_art_game.rock()
+    elif computer_input == 1:
+        rps_art_game.paper()
+    else:
+        rps_art_game.scissors()
+        
+    # Game rules:
+        
+    if user_input == computer_input:   # rock vs rock or paper vs paper or scissors vs scissors = draw
+        print("Draw")
+    elif user_input == 0 and computer_input == 1: # rock vs paper = paper
+        print("You lose!")
+    elif user_input == 0 and computer_input == 2: # rock vs scissors = rock
+        print("You win")
+    elif user_input == 1 and computer_input == 0: # paper vs rock = paper
+        print("You win")
+    elif user_input == 1 and computer_input == 2: # paper vs scissors = scissors
+        print("You lose")
+    elif user_input == 2 and computer_input == 0: # scissors vs rock = rock
+        print("You lose")
+    elif user_input == 2 and computer_input == 1: # scissors vs paper = scissors
+        print("You win")
+    else: 
+        print("Error!")
+else:
+    print("Sorry! Invalid option, try again.")
---
+++ day04/rps_art_game.py
@@ -0,0 +1,40 @@
+# Rock Paper Scissors ASCII Art
+
+def rock():
+# Rock
+    return print('''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+    ''')
+
+def paper():
+    # Paper
+    return print('''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+
+    ''')
+
+def scissors():
+    # Scissors
+    return print('''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+    ''')
+
+# Credits:
+# - RPS Hands by Veronica Karlsson, https://www.asciiart.eu/people/body-parts/hand-gestures
