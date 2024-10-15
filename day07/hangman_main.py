4cdab42fc476eba5c3bd557c6619dcb520570c5e
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-10-14 23:34:48 -0300

    Hangman game
    hangman challenger game

---
+++ day07/hangman_main.py
@@ -0,0 +1,55 @@
+import random
+from hangman_words import word_list
+from hangman_art import stages
+
+lives = 6
+hangman_count = 0
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+game_over = False
+
+for position in range(len(chosen_word)):
+    placeholder += "_"
+
+print(placeholder)
+
+correct_letters = []
+used_letters = []
+
+while not game_over:
+
+    print(f"{lives}/6 Lives left")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in used_letters:
+        print(f"You've already guessed this letter: {guess}")
+        continue
+    else:
+        used_letters.append(guess)
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    if guess not in chosen_word:
+        lives -= 1
+        print("Sorry! You lose a life!")
+        if lives == 0:
+            game_over = True
+            print(f"You lose! It was '{chosen_word}'")
+
+    if "_" not in display:
+        game_over = True
+        print("You win")
+
+    print(stages[lives])
---
+++ day07/hangman_words.py
@@ -0,0 +1,4 @@
+word_list = ['parada', 'laranja', 'faculdade', 'limpeza', 'trabalhar', 'entrega', 'comida', 'estrada', 'casa', 'viagem',
+             'pessoa', 'energia', 'dinheiro', 'rapido', 'febre', 'peixe', 'carro', 'tarde', 'janela', 'cadeira']
+
+
