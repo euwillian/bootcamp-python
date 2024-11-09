1248ed24bb2fe0ca05a35ea16d601ac07944b350
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-11-09 11:15:20 -0300

    day10

---
+++ day10/functions.py
@@ -0,0 +1,32 @@
+"""
+Functions
+
+simple:
+def simple_function():
+    #Do this
+    #Then do this
+    #Finally do this
+
+simple_function()
+
+with inputs:
+def simple_function(something):
+    #Do this with something
+    #Then do this
+    #Finally do this
+
+simple_function(123)
+
+with outputs:
+def simple_function(something):
+    return 1 + 1
+
+output = simple_function()
+
+"""
+
+# tasks
+
+def format_name():
+    pass
+
