5e2463dd1f5a6bfaa6f0614d35746f60a6456cfe
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-11-12 19:54:45 -0300

    Update calculator-project.py

--- day10/calculator-project.py
+++ day10/calculator-project.py
@@ -18,29 +18,24 @@ def mul(n1, n2) -> float:
     return n1 * n2
 
 
-def operation(op):
-    if op == "+":
-        return add(first_number, second_number)
-    elif op == "-":
-        return sub(first_number, second_number)
-    elif op == "*":
-        return mul(first_number, second_number)
-    elif op == "/":
-        return div(first_number, second_number)
-    else:
-        return "Invalid Operation!"
-
+operations = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div
+}
 
 while True:
+
     first_number = float(input("First number: "))
     op_choosed = input("+ - * /:  ")
     second_number = float(input("Second number: "))
 
-    result = operation(op_choosed)
+    result = operations[op_choosed](first_number, second_number)  # operations[]()
 
-    print(f"Result: {result} continue calculating with the last result? type y/n")
+    continue_calc = input(f"Result: {result} continue calculating with the last result? type y/n")
 
-    if result == "y":
+    if continue_calc == "y":
         pass
     else:
         break
