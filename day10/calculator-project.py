55664a5bba76ea485f8a7919ee2b247a4e7d6886
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-11-13 23:18:37 -0300

    Update calculator-project.py

--- day10/calculator-project.py
+++ day10/calculator-project.py
@@ -1,4 +1,7 @@
 # add
+import os
+
+
 def add(n1, n2) -> float:
     return n1 + n2
 
@@ -18,26 +21,33 @@ def mul(n1, n2) -> float:
     return n1 * n2
 
 
-first_number = float(input("First number: "))
+def calculator():
+    should_accumulate = True
+    first_number = float(input("First number: "))
 
-while True:
+    while should_accumulate:
 
-    operations = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": div
-    }
+        operations = {
+            "+": add,
+            "-": sub,
+            "*": mul,
+            "/": div
+        }
 
-    operation_symbol = input("+ - * /:  ")
-    second_number = float(input("Second number: "))
+        operation_symbol = input("+ - * /:  ")
+        second_number = float(input("Second number: "))
 
-    result = operations[operation_symbol](first_number, second_number)  # operations[]()
+        result = operations[operation_symbol](first_number, second_number)  # operations[]()
 
-    print(f"{first_number} {operation_symbol} {second_number} = {result}")
-    continue_calc = input(f"Result: {result} continue calculating with the last result? type y/n: ")
+        print(f"{first_number} {operation_symbol} {second_number} = {result}")
+        continue_calc = input(f"Result: {result} continue calculating with the last result? type y/n: ")
 
-    if continue_calc == "y":
-        first_number = result
-    else:
-        break
+        if continue_calc == "y":
+            first_number = result
+        else:
+            should_accumulate = False
+            os.system("cls")
+            calculator()  # To continuoes runnning this program
+
+
+calculator()  # To run the program, for the first time
