65719dfb3a0944557cd3eeb467e7e24f7d7fc1ac
Willian Santos <12131283+euwillian@users.noreply.github.com>
2024-11-18 20:05:22 -0300

    blackjack-game

---
+++ day11/blackjack-game.py
@@ -0,0 +1,36 @@
+"""
+Our BlackJack Game Rules:
+
+- the deck is unlimited in size
+- there are no jokes
+- the Jack/Queen/King all count as 10
+- The Ace can count as 11 or 1
+- Use the following list as the deck of cards:
+
+cards = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+- The cards in the lista have equal probabily of being drawn
+- Cards are not removed from the deck as they are drawn
+- The computer is the Dealer
+
+
+"""
+
+cards = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+var1 = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+"""
+Your cards: [10,4], current score: 14
+    Computer's first card: 5
+Type 'y' to get another card, type 'n' to pass: n
+
+
+--
+
+Your final hand: [10,4,5], final score: 19
+   Computer's final hand: [5,6,1,3,10], final score: 25
+Opponent went over. You win 😁
+Do you want to play a game of Blackjack? Type 'y' or 'n':
+
+
+"""
